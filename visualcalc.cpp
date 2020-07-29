c57bc9cc5d67c40ee3730d31f94e631851dc10a5
Ruixin-Wang <wangrx1999@gmail.com>
2020-07-29 22:32:23 +0800

    Revert "Revert "matrix fix, fix cos->de()""
    This reverts commit e9746b119f4a28ae79bd82b3cf5d386ad382196a.

--- visualcalc.cpp
+++ visualcalc.cpp
@@ -24,7 +24,7 @@ bool x_enable, y_enable;
 
 
 std::map<std::string, double> variables;
-
+std::map<std::string, MATRIX*> variablesforMatrix;
 
 VisualCalc::VisualCalc(QWidget* parent)
     : QMainWindow(parent),
@@ -336,6 +336,7 @@ void VisualCalc::IntegrationEqualButtonPressed() {
     ui->Diff_Val->setText(QString(""));
     ui->Val->setText(QString(""));
     this->Tree->clear();
+    varTrig = false;
 }
 
 void VisualCalc::DerivateEqualButtonPressed() {
@@ -393,6 +394,7 @@ void VisualCalc::DerivateEqualButtonPressed() {
     ui->Diff_Val->setText(QString::number(solution));
     ui->Val->setText(QString("READY"));
     this->Tree->clear();
+    varTrig = false;
 }
 
 void VisualCalc::EqualButtonPressed() {
