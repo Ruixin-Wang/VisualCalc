fbe843ce4786687dfc3aa8943b07c42e750c8357
One-punch24 <63431994+One-punch24@users.noreply.github.com>
2020-07-29 08:47:23 +0800

    Introduce culculus

---
+++ calculus.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"Node.h"
+
+double NumIntegrationSimpson(ExprTree* T, double low_bound, double up_bound);
+double NumIntegrationRomberg(ExprTree* T, double low_bound, double up_bound);
+double NumDiff( ExprTree* T, double point);
+double ExprDiff(ExprTree* T, double point);
